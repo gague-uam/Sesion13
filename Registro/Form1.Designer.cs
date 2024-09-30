c62efd460932881e8a0d88f18eb0645afd9d95fc
gague-UAM <ggpaiz@uamv.edu.ni>
2024-09-30 08:45:55 -0600

    30 de Septiembre Segundo Ejercicio

---
+++ Registro/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Registro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbNames = new System.Windows.Forms.TextBox();
+            this.tbLastNames = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.dtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.dgvPersons = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(35, 38);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(52, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombres:";
+            // 
+            // tbNames
+            // 
+            this.tbNames.Location = new System.Drawing.Point(87, 34);
+            this.tbNames.Name = "tbNames";
+            this.tbNames.Size = new System.Drawing.Size(286, 20);
+            this.tbNames.TabIndex = 1;
+            // 
+            // tbLastNames
+            // 
+            this.tbLastNames.Location = new System.Drawing.Point(87, 60);
+            this.tbLastNames.Name = "tbLastNames";
+            this.tbLastNames.Size = new System.Drawing.Size(286, 20);
+            this.tbLastNames.TabIndex = 3;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(35, 64);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(52, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Apellidos:";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(32, 96);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(111, 13);
+            this.lblBirthDate.TabIndex = 4;
+            this.lblBirthDate.Text = "Fecha de Nacimiento:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Location = new System.Drawing.Point(144, 94);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(229, 20);
+            this.dtpBirthDate.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(64, 140);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(104, 34);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(236, 140);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(103, 31);
+            this.btnOrder.TabIndex = 7;
+            this.btnOrder.Text = "Ordenar";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            // 
+            // dgvPersons
+            // 
+            this.dgvPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersons.Location = new System.Drawing.Point(64, 203);
+            this.dgvPersons.Name = "dgvPersons";
+            this.dgvPersons.Size = new System.Drawing.Size(275, 159);
+            this.dgvPersons.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 396);
+            this.Controls.Add(this.dgvPersons);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.tbLastNames);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.tbNames);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbNames;
+        private System.Windows.Forms.TextBox tbLastNames;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.DateTimePicker dtpBirthDate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.DataGridView dgvPersons;
+    }
+}
+
---
+++ Registro/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Registro.modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro
+{
+    public partial class Form1 : Form
+    {
+        List<Person> persons = new List<Person>();  
+        public Form1()
+        {
+            InitializeComponent();
+            persons = new List<Person>();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.FirstName = tbNames.Text;
+            person.LastName = tbLastNames.Text;
+            person.BirthDate = dtpBirthDate.Value;
+            persons.Add(person);
+            ShowPerson();
+            ClearTb();
+        }
+        private void ShowPerson()
+        {
+            dgvPersons.DataSource = null;
+            dgvPersons.DataSource = persons;
+        }
+
+        private void ClearTb()
+        {
+            tbNames.Clear();
+            tbLastNames.Clear() ;
+            dtpBirthDate.Value = DateTime.Now;
+            tbNames.Focus();
+        }
+    }
+}
